7433ee7d977485157099f0bad82f398f1e6a7cc2
dydtjr0316 <50666106+dydtjr0316@users.noreply.github.com>
2021-09-13 22:58:45 +0900

    포트폴리오_쿼드트리 공부

---
+++ forHD/first/first/first.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//  ; = 세미콜론
+	//	i != 7 i가 7이 아닐때
+	// bool 은 true or false 
+	bool b = true;
+//	if (/*조건의 자리에 오는 문장은 무조건 true or false이다.*/)
+
+
+	// 이코드에서 앞에오는 숫자가 i 뒤에오는 숫자가 j
+	// 1x7 2x7 3x7 처럼 뒤에 7을 곱하는 친구들을 출력하기 싫어
+	// 1.
+	/*for (int i = 1; i < 10; i++)
+	{
+		for (int j = 1; j < 10; j++)
+		{
+			if (i != 7)
+				cout << i << " X " << j << " = " << i * j << endl;
+		}
+		cout << "-------------------" << endl;
+	}*/
+
+
+	// 2.
+	// break -> 반복문 or 루프 하나를 빠져나가는 명령어
+	// continue -> 
+	//for (int i = 1; i < 10; i++)
+	//{
+	//	for (int j = 1; j < 10; j++)
+	//	{
+	//		if (j == 7) 
+	//			continue;
+	//		cout << i << " X " << j << " = " << i * j << endl;
+	//	}
+	//	cout << "-------------------" << endl;
+	//}
+	
+	// while문으로 바꿔서 구구단 출력
+	// if문도 switch로 바꿔
+	// do - while 공부
+	// 지역 변수 전역 변수 공부 - 
+}
